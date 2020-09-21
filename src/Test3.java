3037ef4a079011a512aa8536339cb46ef3e289b7
isuru <isurujayawickramat@gmail.com>
2020-09-21 21:51:09 +0530

    Initial commit

---
+++ src/Test3.java
@@ -0,0 +1,6 @@
+public class Test3 {
+    // by changing sequence of the modifiers, method prototype is not changed.
+    static public void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
